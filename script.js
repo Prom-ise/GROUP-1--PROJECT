004ee5a46361f3d117fc7c9775a6c13fadc99e5e
promise <emmanuelpromise456@gmail.com>
2024-02-12 09:17:31 +0100

    Updated a file

---
+++ script.js
@@ -0,0 +1,52 @@
+{/* <div class="d-flex hide shadow-lg">
+                      <ul>
+                        <div>FOOD CUPBOARD</div>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a href="#">Grains (e.g Rice)</a></li>
+                        <li><a href="#">Breakfast Foods</a></li>
+                        <li><a href="#">Herbal, Spices & Seasonings</a></li>
+                        <li><a href="#">Flours & Meal</a></li>
+                        <li><a href="#">Malt Drinks</a></li>
+                        <li><a href="#">Noodles & Pasta</a></li>
+                        <li><a href="#">Coffee</a></li>
+                        <li><a href="#">Bottle Water</a></li>
+                        <li><a href="#">Cooking Oil (All various types..)</a></li>
+                        <li><a href="#">Snacks</a></li>
+                        <li><a href="#">Canned and Packaged Foods</a></li>
+                        <li><a href="#">Chocolate & Candy</a></li>
+                        <li><a href="#">Grocerries</a></li>
+                        <li><a href="#">Cabbages</a></li>
+                        <li><a href="#">Syrups</a></li>
+                        <li><a href="#">Fruits</a></li>
+                      </ul>
+    
+                      <div class="d-flex">
+                      <ul>
+                        <div>BEVERAGES</div>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a href="#">Soft Drink</a></li>
+                        <li><a href="#">Milk & Cream</a></li>
+                        <li><a href="#">Energy Drink</a></li>
+                        <li><a href="#">Bottled Beverages</a></li>
+                      </ul>
+                      <ul>
+                        <div>HOUSEHOLD CLEANING</div>
+                        <li><hr></li>
+                        <li><a href="#">Air Fresheners</a></li>
+                        <li><a href="#">Cleaning Tools</a></li>
+                        <li><a href="#">Laundry</a></li>
+                        <li><a href="#">Toilet Paper</a></li>
+                        <li><a href="#">Bathroom Cleaners</a></li>
+                        <li><a href="#">Toilet Paper & Wipes</a></li>
+                      </ul>
+                      <ul>
+                      <div>BEER, WINE & SPIRITS</div>
+                      <li><hr></li>
+                      <li><a href="#">Beers</a></li>
+                      <li><a href="#">Vodka</a></li>
+                      <li><a href="#">Red or Blue Wine</a></li>
+                      <li><a href="#">Trophy</a></li>
+                      <li><a href="#">Hennessy</a></li>
+                      </ul>
+                      </div>
+                    </div> */}
